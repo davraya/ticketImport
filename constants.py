6e7b2b9bbff6fda4b4727dac9595ed2ef4dca1ed
David Raya <85646734+davraya@users.noreply.github.com>
2022-09-19 11:41:27 -0600

    Working program

--- constants.py
+++ constants.py
@@ -1,36 +1,64 @@
-FILE_NAME = "One Form - Phone Backup (2)(1-4).xlsx"
+FILE_NAME = "One Form - Phone Backup (3)(1-2).xlsx"
 SHEET_NAME = "Sheet1"
 
 
 # Values are Column names as they are on Microsoft Form
 
-contact = "Contact's Name"
+unknown = "Unknown"
 i_number = "I-Number"
 department = "What area is this issue about?"
 notes = "Contact Notes"
 
-ACTIONS = [f"Which action did you use to help the contact?{n}" for n in range (2, 11)]
-
-
-
 COLUMNS = [
-    contact,
+    unknown,
     i_number,
     department, 
     notes
     ] 
 
-# Values are One Form attributes names
+email_dept_match = {
+    "Pathway Student Unknown" : ["unknownpathway@unknown.com","PathwayOnlineStudent"],
+    "On Campus Student Unknown" : ["campusunknown@unknown.com", "OnTrackStudent"], 
+    "Online Student Unknown" : ["onlineunknown@unknown.com", "OnlineStudent"],
+    "Online Prospective Student Unknown" : ["onlineprospectiveunknown@unknown.com", "OnlineProspectiveStudent"],
+    "Community Member Unknown" : ["communitymember@unknown.com", "Community Member"],
+    "Parent/Other Relative Unknown" : ["Parentunknown@unknown.com", "Parent/Other Relative"],
+    "Alumni/Former BYUI Student Unknown" : ["alumniunknown@unknown.com", "Alumni"],
+    "Employee/Faculty Unknown" : ["employeeunknown@unknown.com", "Student Services"],
+    "Vendor ." : ["vendor@unknown.com", "Vendor"],
+    "Church Leader Unknown" : ["churchleader@unknown.com", "Bishop/Stake Pres"],
+    "Missionary Unknown" : ["missionaryunknown@unknown.com", "MissionaryDeferredStudent"],
+}
+
+contact_type = {
+    "Pathway Student Unknown" : "PathwayOnlineStudent",
+    "On Campus Student Unknown" : "OnTrackStudent",
+    "Online Student Unknown" : "OnlineStudent",
+    "Online Prospective Student Unknown" : "OnlineProspectiveStudent",
+    "Community Member Unknown" : "Community Member",
+    "Parent/Other Relative Unknown" : "Parent/Other Relative",
+    "Alumni/Former BYUI Student Unknown" : "Alumni",
+    "Employee/Faculty Unknown" : "Student Services",
+    "Vendor ." : "Vendor",
+    "Church Leader Unknown" : "Bishop/Stake Pres",
+    "Missionary Unknown" : "MissionaryDeferredStudent"
+}
+
+
+# Values are One Form attributes in TD
 
 ATTRIBUTES ={
-    contact : "Requestor",
+    unknown : "Requestor",
     i_number : "I-Number (Back Up Phone Form)",
     department : "Office List 1",
-    notes : "Contact Notes"
+    notes : "Contact Notes",
 }
 
 TD_ACTION = "Action Options - Phones"
-
+ACTIONS = ["Which action did you use to help the contact?"] 
+ACTIONS += [f"Which action did you use to help the contact?{n}" for n in range (2, 11)]
 ACTION_DICT = {column : TD_ACTION for column in ACTIONS}
+
+
 
 
